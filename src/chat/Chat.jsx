d31de2a930c4827473836139bc0d54587397d203
Kauã Machado Grathwohl <kaua.machado@emasoftware.com.br>
2023-11-09 18:21:32 -0300

    Adicionando alterações nas páginas principais

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -1,27 +1,101 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import "./chat.css";
 
-function Button(props) {
+function Chat() {
+    const [mensagens, setMensagens] = useState([
+        (
+            <div key={0}>
+                <div className="msg-atendente">
+                    <strong>Atendente:</strong>
+                </div>
+                <div className="msg mensagem-atendente">Olá! Como posso ajudar?</div>
+            </div>
+        ),
+        (
+            <div key={1}>
+                <div className="msg-aluno">
+                    <strong>Você:</strong>
+                </div>
+                <div className="msg mensagem-aluno">Olá! Gostaria de tirar uma dúvida referente ao curso de Engenharia de Software!</div>
+            </div>
+        ),
+        (
+            <div key={2}>
+                <div className="msg-atendente">
+                    <strong>Atendente:</strong>
+                </div>
+                <div className="msg mensagem-atendente">Certo! Qual seria a dúvida?</div>
+            </div>
+        ),
+    ]);
 
-  console.log(props);
+    const [mensagem, setMensagem] = useState("");
+    const chatMensagemRef = useRef(null);
 
-  return (
-    <button onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+    const adicionarMensagem = (nome, texto, classe) => {
+        const novaMensagem = (
+            <div key={mensagens.length}>
+                <div className={`msg-${classe}`}>
+                    <strong>{nome} diz:</strong>
+                </div>
+                <div className={`msg mensagem-${classe}`}>{texto}</div>
+            </div>
+        );
+
+        setMensagens((mensagensAntigas) => [...mensagensAntigas, novaMensagem]);
+    };
+
+    const enviarMensagem = () => {
+        if (mensagem.trim() !== "") {
+            adicionarMensagem("Você", mensagem, "aluno");
+            setMensagem("");
+        } else {
+            alert("Digite uma mensagem!");
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            enviarMensagem();
+        }
+    };
+
+    useEffect(() => {
+        const chatMensagem = chatMensagemRef.current;
+        if (chatMensagem) {
+            chatMensagem.scrollTop = chatMensagem.scrollHeight;
+        }
+    }, [mensagens]);
+
+    return (
+        <div className="chat-box">
+            <div className="chat-header">
+                <h2>Atendimento Online</h2>
+            </div>
+
+            <div className="chat-mensagem" ref={chatMensagemRef}>
+                {mensagens}
+            </div>
+
+            <div className="input-box">
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    enviarMensagem();
+                }}>
+                    <input
+                        id="inputMensagem"
+                        value={mensagem}
+                        onChange={(e) => setMensagem(e.target.value)}
+                        onKeyDown={(event) => handleKeyDown(event)}
+                        type="text"
+                        placeholder="Digite sua mensagem..."
+                    />
+                    <button id="buttonEnviar" type="submit">Enviar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-function Chat(props) {
-
-  function handleClick() {
-    console.log('handleClick')
-  }
-
-  return (
-    <Button onClick={handleClick}>
-      botao1
-    </Button>
-  );
-}
-
-export default Chat
+export default Chat;
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,32 +1,40 @@
-import Input from "../components/input/Input";
-import Button from "../components/button/Button";
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import Input from '../components/input/Input';
+import Button from '../components/button/Button';
 import './login.css';
 
 function Login() {
   const [username, setUsername] = useState('');
-  const history = useNavigate();
+  const Navigate = useNavigate();
 
   const handleLogin = () => {
-
     if (username.trim() !== '') {
+      alert(`Bem-vindo ${username}!`);
+      Navigate('/chat');
 
-      alert(`Bem-vindo, ${username}!`);
-      history.push('/chat');
     } else {
-
       alert('Por favor, insira um nome de usuário válido.');
+    }
+  };
+
+  const handleKeyDown = (event) => {
+
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleLogin();
     }
   };
 
   return (
     <div className="container">
-
       <h1>Login</h1>
-      <Input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <Input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
       <Button onClick={handleLogin} />
-
     </div>
   );
 }

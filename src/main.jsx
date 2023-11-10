4a02fc700822a523758099470bbcbb83566ac414
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-10 11:32:15 -0300

    adiciona ant-design

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
 import Router from './router/Router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <ConfigProvider locale={ptBR}>
+      <Router />
+    </ConfigProvider>
   </React.StrictMode>,
 )

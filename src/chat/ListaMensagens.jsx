85d2757b43c26317c8f1e5fe59f4797f85da26ce
Augusto Tomasi <augustopreisthomasi@gmail.com>
2023-11-13 08:58:48 -0300

    adiciona scroll no chat

--- src/chat/ListaMensagens.jsx
+++ src/chat/ListaMensagens.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Mensagem from './Mensagem';
 
 function ListaMensagens({ mensagens }) {
+  const mensagensRef = useRef(null);
+
+  useEffect(() => {
+    if (mensagensRef.current) {
+      mensagensRef.current.scrollTop = mensagensRef.current.scrollHeight;
+    }
+  }, [mensagens]);
 
   return (
-    <div className='chat-mensagem'>
-      {mensagens?.map((msg, i) => (
-        <Mensagem key={i}
-          atendente={msg.atendente}
-          texto={msg.texto} />
-      ))}
+    <div className='chat-mensagem'
+      ref={mensagensRef}>
+      <div style={{ maxHeight: '55vh' }}>
+        {mensagens?.map((msg, i) => (
+          <Mensagem key={i}
+            atendente={msg.atendente}
+            texto={msg.texto} />
+        ))}
+      </div>
     </div>
   );
 }
